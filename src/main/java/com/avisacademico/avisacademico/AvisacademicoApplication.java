56e55d38b9e8413f490a6aa24e6f6eddc9ae1e0c
m-brito <britomauricio2003@gmail.com>
2024-04-26 02:55:54 -0300

    :seedling: Project initialization

---
+++ src/main/java/com/avisacademico/avisacademico/AvisacademicoApplication.java
@@ -0,0 +1,13 @@
+package com.avisacademico.avisacademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisacademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisacademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avisacademico/avisacademico/AvisacademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avisacademico.avisacademico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvisacademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
