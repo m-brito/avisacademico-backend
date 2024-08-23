c7caef67af79ffc2900acf607c3756ef7407cdce
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-08-23 08:13:48 -0300

    refactor: modularize project structure

--- src/main/java/com/avisacademico/Application.java
+++ src/main/java/com/avisacademico/Application.java
@@ -1,13 +1,13 @@
-package com.avisacademico.avisacademico;
+package com.avisacademico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AvisacademicoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AvisacademicoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
