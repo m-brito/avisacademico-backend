ba1bcb4d9f4439f22936539d7bdc13e0efc6fae1
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-08-29 20:16:49 -0300

    :sparkles: feat: create user and role repositories

---
+++ src/main/java/com/avisacademico/domain/repository/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.avisacademico.domain.repository.user;
+
+import com.avisacademico.domain.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<Long, User> {
+    UserDetails findByUsername(String username);
+}
