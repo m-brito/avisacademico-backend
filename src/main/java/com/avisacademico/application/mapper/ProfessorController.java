5fe219316a06f2ca4292b3b94b605c045a623f31
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-08-30 02:40:51 -0300

    refactor: changing name of teacher route

--- src/main/java/com/avisacademico/application/mapper/ProfessorController.java
+++ src/main/java/com/avisacademico/application/mapper/ProfessorController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/register-teacher")
+@RequestMapping("/cadastrar-professor")
 public class ProfessorController {
 
     private final ProfessorService professorService;
